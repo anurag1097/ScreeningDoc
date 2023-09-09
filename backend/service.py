58613f89e61059a1e0e522de57fa29c5e48f4f98
anurag <asharma7_be15@thapar.edu+anurag1097@users.noreply.github.com>
2023-09-09 12:19:27 +0530

    ScreeningDoc --> Updated requirements.txt file and backend service

--- backend/service.py
+++ backend/service.py
@@ -64,5 +64,4 @@ def fetch_results_based_on_input():
 
 
 if __name__ == "__main__":
-    # app.run(host=constants["HOST"], port=constants["PORT"])
-    app.run(debug=False, host="0.0.0.0")
+    app.run(host=constants["HOST"], port=constants["PORT"])
