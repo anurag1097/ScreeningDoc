2b0931abcefa399be6e8e794534ce5d543900fe3
anurag <asharma7_be15@thapar.edu+anurag1097@users.noreply.github.com>
2023-09-09 20:51:34 +0530

    Fix: Mobile UI

--- screening_doc_ui/src/App.js
+++ screening_doc_ui/src/App.js
@@ -7,6 +7,7 @@ import HomePage from './components/HomePage'
 import AboutUs from './components/AboutUs';
 
 function App() {
+
   const [isAuthenticated, setIsAuthenticated] = useState(false);
   const navigate = useNavigate();
   const logout = () => {
@@ -36,12 +37,31 @@ function App() {
           }
         />
       </Routes>
-
     </InactivityDetector>
   );
 }
 
 function HomeAndSignUpLayout({ isAuthenticated, setIsAuthenticated }) {
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    // Function to check if the screen width is below a certain threshold (e.g., 768px)
+    const checkIsMobile = () => {
+      setIsMobile(window.innerWidth < 768); // Adjust the threshold as needed
+    };
+
+    // Initially check on component mount
+    checkIsMobile();
+
+    // Add a listener to check whenever the window is resized
+    window.addEventListener('resize', checkIsMobile);
+
+    // Clean up the listener when the component unmounts
+    return () => {
+      window.removeEventListener('resize', checkIsMobile);
+    };
+  }, []);
+
   const [showAbout, setShowAbout] = useState(false);
 
   const handleScroll = () => {
@@ -57,15 +77,32 @@ function HomeAndSignUpLayout({ isAuthenticated, setIsAuthenticated }) {
 
   return (
     <div>
-        <div className="flex-container" style={{"display": "flex", "justifyContent": "space-between", "align-items": "center", "height": "100vh", "padding": "20px"}}>
-          <HomePage />
-          <div className="vertical-line" style={{"width": "1px", "height": "80%", "background-color": "#ccc", "margin": "0 20px"}}/>
-          <SignInSignUpPage
-              isAuthenticated={isAuthenticated}
-              setIsAuthenticated={setIsAuthenticated}
-            />
-        </div>
-        <AboutUs />
+        {
+            !isMobile ? (
+                <div>
+                <div className="flex-container" style={{"display": "flex", "justifyContent": "space-between", "align-items": "center", "height": "100vh", "padding": "20px"}}>
+                  <HomePage />
+                  <div className="vertical-line" style={{"width": "1px", "height": "80%", "background-color": "#ccc", "margin": "0 20px"}}/>
+                  <SignInSignUpPage
+                      isAuthenticated={isAuthenticated}
+                      setIsAuthenticated={setIsAuthenticated}
+                    />
+                </div>
+                <AboutUs isMobile={isMobile}/>
+                </div>
+            ) : (
+                <div>
+                <HomePage />
+                <SignInSignUpPage
+                  isAuthenticated={isAuthenticated}
+                  setIsAuthenticated={setIsAuthenticated}
+                />
+                <div style={{"marginTop": "20%"}}>
+                    <AboutUs isMobile={isMobile} />
+                </div>
+                </div>
+            )
+        }
     </div>
   );
 }
